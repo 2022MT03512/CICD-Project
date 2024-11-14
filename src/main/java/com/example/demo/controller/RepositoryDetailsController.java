fce423bc39a446361f607dc8a2ca4b523bf4deab
2022MT03512 <150220370+2022MT03512@users.noreply.github.com>
2024-11-14 13:37:57 +0530

    Update RepositoryDetailsController.java

--- src/main/java/com/example/demo/controller/RepositoryDetailsController.java
+++ src/main/java/com/example/demo/controller/RepositoryDetailsController.java
@@ -34,7 +34,7 @@ public class RepositoryDetailsController {
 	public String getRepos() throws IOException {
 		GitHub github = new GitHubBuilder().withPassword("valaxytech@gmail.com", "XXXXXXXX").build();
 		GHRepositorySearchBuilder builder = github.searchRepositories();
-		return "Greetings from Valaxy Technologies";
+		return "Greetings from Rakesh Sharma (2022MT03512) "BITS-CICD-PROJECT";
 	}
 
 	@GetMapping("/trends")
